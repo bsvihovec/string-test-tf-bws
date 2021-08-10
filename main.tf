159835f7280b116add90ca0a4a0d3fb013c66e35
bsvihovec <bsvihovec@yahoo.com>
2021-08-10 12:58:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 # Create an arbitrary local resource
 resource "null_resource" "echo" {
-  triggers {
-    uuid = "${uuid()}"
-  }
-
   provisioner "local-exec" {
     command = "echo ${var.sample_var}"
   }
